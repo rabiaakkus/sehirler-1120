dadf36bbfe772bf12eb13675552f6f96b6695bfa
rabiaakkus <145451092+rabiaakkus@users.noreply.github.com>
2023-12-04 11:50:59 +0300

    Add files via upload

---
+++ oyuncu1120/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace oyuncu1120
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSehirler = new System.Windows.Forms.TextBox();
+            this.lblSehir = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lbListe = new System.Windows.Forms.ListBox();
+            this.btnListe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSehirler
+            // 
+            this.txtSehirler.Location = new System.Drawing.Point(89, 41);
+            this.txtSehirler.Name = "txtSehirler";
+            this.txtSehirler.Size = new System.Drawing.Size(100, 20);
+            this.txtSehirler.TabIndex = 0;
+            // 
+            // lblSehir
+            // 
+            this.lblSehir.AutoSize = true;
+            this.lblSehir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSehir.Location = new System.Drawing.Point(2, 37);
+            this.lblSehir.Name = "lblSehir";
+            this.lblSehir.Size = new System.Drawing.Size(81, 24);
+            this.lblSehir.TabIndex = 1;
+            this.lblSehir.Text = "şehir adı";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(6, 92);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(100, 50);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lbListe
+            // 
+            this.lbListe.FormattingEnabled = true;
+            this.lbListe.Location = new System.Drawing.Point(195, 37);
+            this.lbListe.Name = "lbListe";
+            this.lbListe.Size = new System.Drawing.Size(120, 147);
+            this.lbListe.TabIndex = 3;
+            // 
+            // btnListe
+            // 
+            this.btnListe.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListe.Location = new System.Drawing.Point(348, 41);
+            this.btnListe.Name = "btnListe";
+            this.btnListe.Size = new System.Drawing.Size(113, 50);
+            this.btnListe.TabIndex = 4;
+            this.btnListe.Text = "LİSTELE";
+            this.btnListe.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 220);
+            this.Controls.Add(this.btnListe);
+            this.Controls.Add(this.lbListe);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lblSehir);
+            this.Controls.Add(this.txtSehirler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSehirler;
+        private System.Windows.Forms.Label lblSehir;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lbListe;
+        private System.Windows.Forms.Button btnListe;
+    }
+}
+
